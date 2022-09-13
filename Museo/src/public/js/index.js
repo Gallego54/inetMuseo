428b12b0635de1faa03c036baf629620d0064ac6
G4ll <lolcuenta2000@gmail.com>
2022-09-13 12:16:01 -0300

    asd

--- Museo/src/public/js/index.js
+++ Museo/src/public/js/index.js
@@ -1,9 +1,13 @@
 
 window.onload = () => {
     consumeAPI('https://api.github.com/users/manishmshiva').then(json => console.log(json));
+    /*addPoint(1);
     addPoint(1);
-    addPoint(1);
-    addPoint(1);
+    addPoint(1);*/
+
+    firstIndexRender();
+  
+
 }
 
 const textToVoice = text => {
@@ -21,4 +25,25 @@ const addPoint = ( n ) => {
     Map.innerHTML += `<button class="">${n}</button>`;
 }
 
+
+
+const firstIndexRender =  () => {
+    listenerAdder('sw-button', 'click', ()=>{
+        classAdder('sw-button', 'hidden-animation');
+        classAdder('sw-text', 'hidden-animation');
+
+        classAdder('sw-img', 'swipe-animation');
+
+        classAdder('article-index', 'show-animation');
+        classAdder('nav', 'show-animation'); 
+    })
+}
+
+const listenerAdder = ( id, event, func ) => {
+    document.getElementById(id).addEventListener(event, func);
+}
+
+const classAdder = ( id, className ) => {
+    document.getElementById(id).classList.add(className);
+}
 
