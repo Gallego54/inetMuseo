0c1ef465fb4e9d74c7e2283fd13abbbac296f727
you@example.com <you@example.com>
2022-09-13 19:36:31 -0300

    tuka es un genio de las bases de datos sin duda

--- Museo/src/public/js/index.js
+++ Museo/src/public/js/index.js
@@ -5,45 +5,128 @@ window.onload = () => {
     addPoint(1);
     addPoint(1);*/
 
-    firstIndexRender();
-  
+
+    document.getElementById ("access").addEventListener
+        ('click', ()=>{ 
+            renderUserform()
+        })
 
 }
+
+
+
+
+
+
+const firstIndexRender =  () => {
+    const manager = new ElementManagement;
+
+    manager.listenerAdder('sw-button', 'click', ()=>{
+        manager.classAdder('sw-button', 'hidden-animation');
+        manager.classAdder('sw-text', 'hidden-animation');
+
+        manager.classAdder('sw-img', 'swipe-animation');
+
+        manager.classAdder('article-index', 'show-animation');
+        manager.classAdder('nav', 'show-animation'); 
+    })
+}
+
+
+const renderUserform = () => {
+    const generator = new ElementGenerator;
+    const manager = new ElementManagement;
+
+    generator.removeAllElements();
+
+    manager.classRemover("home", 'active');
+    manager.classAdder("access", 'active');
+    const text = generator.makeElement("input", {type: "text", class:"input-text"},  [generator.makeElement("input", {type: "text", class:"input-text"}, ["asd"])]  );
+    console.log(text)
+
+    /*generator.getRoot()
+    .appendChild(generator.makeElement("div", {class: "form-container"}, [
+        ,
+        generator.makeElement("input", {class: "form-password", type: "password"})
+    ]))
+*/}   
+
+
+/*
+var google = createElement("a",{"href":"http://google.com"},"google"),
+    youtube = createElement("a",{"href":"http://youtube.com"},"youtube"),
+    facebook = createElement("a",{"href":"http://facebook.com"},"facebook"),
+    links_conteiner = createElement("div",{"id":"links"},[google,youtube,facebook]);
+*/ 
+const ElementGenerator = function () {
+    const root = document.getElementById("root");
+
+    this.makeElement = ( tag, atrib, content ) => {
+        const element = document.createElement(tag);
+        const elementWithAtrib = pushAttrib (element, atrib); 
+        const elementWithContent = pushContent (element, content); 
+
+        return elementWithContent;
+    }
+
+    this.getRoot = () => {
+        return root;
+    }
+
+    this.removeAllElements = () => {
+        while (root.firstChild) {
+          root.removeChild(root.lastChild);
+        }
+    }
+
+
+    const pushAttrib = ( HTMLElement, attribs ) => {
+        const attribName =  Object.keys(attribs);
+        const attribValues =  Object.values(attribs);
+
+        for (let i=0 ; i<attribName.length ; i++) {
+            if (attribName!= 'class') {
+                HTMLElement.setAttribute(attribName[i], attribValues[i])
+            }else{
+                HTMLElement.classList.add(attribValues[i]);
+            }
+
+        }
+
+        return HTMLElement;
+    }
+
+    const pushContent = (HTMLElement, content) => {
+        for (const iterator of content) {
+            if (iterator.nodeName !== undefined) {
+                HTMLElement.appendChild (iterator);
+            } else {
+                HTMLElement.appendChild (document.createTextNode(iterator));
+            }
+            
+        }   
+
+        return HTMLElement;
+    }
+}
+
+const ElementManagement = function () {
+    this.listenerAdder = ( id, event, func ) => {
+        document.getElementById(id).addEventListener(event, func);
+    }
+    
+    this.classAdder = ( id, className ) => {
+        document.getElementById(id).classList.add(className);
+    }
+
+    this.classRemover = ( id, className ) => {
+        document.getElementById(id).classList.remove(className);
+    }
+}
+
 
 const textToVoice = text => {
     /*to do*/
     const synth = window.speechSynthesis
     const utterThis = new SpeechSynthesisUtterance(text)
 }
-
-const getControlPoint = () => {
-
-}
-
-const addPoint = ( n ) => {
-    const Map = document.getElementById('img__mapmuseo');
-    Map.innerHTML += `<button class="">${n}</button>`;
-}
-
-
-
-const firstIndexRender =  () => {
-    listenerAdder('sw-button', 'click', ()=>{
-        classAdder('sw-button', 'hidden-animation');
-        classAdder('sw-text', 'hidden-animation');
-
-        classAdder('sw-img', 'swipe-animation');
-
-        classAdder('article-index', 'show-animation');
-        classAdder('nav', 'show-animation'); 
-    })
-}
-
-const listenerAdder = ( id, event, func ) => {
-    document.getElementById(id).addEventListener(event, func);
-}
-
-const classAdder = ( id, className ) => {
-    document.getElementById(id).classList.add(className);
-}
-
